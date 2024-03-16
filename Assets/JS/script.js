e67aacdebb56a78f7ce1ede1d84a2e203b4badd4
Danayaaa <155805901+Danayaaa@users.noreply.github.com>
2024-03-16 22:26:19 +0800

    Update script.js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){
-    $(".email-login").hide(); 
-    $(".email-signup").show(); 
+    $(".login").hide(); 
+    $(".signup").show(); 
     $("#login-box-link").removeClass("active"); 
     $("#signup-box-link").addClass("active");
 
     $("#signup-box-link").click(function(){
-        $(".email-login").fadeOut(100);
-        $(".email-signup").delay(100).fadeIn(100);
+        $(".login").fadeOut(100);
+        $(".signup").delay(100).fadeIn(100);
         $("#login-box-link").removeClass("active");
         $("#signup-box-link").addClass("active");
     });
     $("#login-box-link").click(function(){
-        $(".email-login").delay(100).fadeIn(100);
-        $(".email-signup").fadeOut(100);
+        $(".login").delay(100).fadeIn(100);
+        $(".signup").fadeOut(100);
         $("#login-box-link").addClass("active");
         $("#signup-box-link").removeClass("active");
     });
 });
-document.querySelector('.email-signup .u-form-group button').addEventListener('click', function(event) {
+document.querySelector('.signup .input button').addEventListener('click', function(event) {
     event.preventDefault();
 
-    var firstName = document.querySelector('.email-signup input[placeholder="first name"]').value;
-    var lastName = document.querySelector('.email-signup input[placeholder="last name"]').value;
-    var password = document.querySelector('.email-signup input[placeholder="Password"]').value;
-    var confirmPassword = document.querySelector('.email-signup input[placeholder="Confirm Password"]').value;
+    var firstName = document.querySelector('.signup input[placeholder="first name"]').value;
+    var lastName = document.querySelector('.signup input[placeholder="last name"]').value;
+    var password = document.querySelector('.signup input[placeholder="Password"]').value;
+    var confirmPassword = document.querySelector('.signup input[placeholder="Confirm Password"]').value;
 
     document.getElementById('modalText').innerHTML = 'First Name:  ' + firstName + '<br>Last Name:  ' + lastName + '<br>Password:  ' + password + '<br>Confirm Password:  ' + confirmPassword;
 
